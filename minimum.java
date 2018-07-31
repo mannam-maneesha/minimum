393c9ef9d39bebf82ff08b5e2f0e63cbeed1457f
mannam-maneesha <41893993+mannam-maneesha@users.noreply.github.com>
2018-07-31 12:12:27 +0530

    Create minimum.java

---
+++ minimum.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{    int min;
+		Scanner s =new Scanner(System.in);
+		int n = s.nextInt();
+		int []a = new int[n];
+		  for(int i = 0; i < n; i++)
+		   {
+			  a[i] = s.nextInt();
+		    }
+		min = a[0];
+		for(int i = 1; i < n; i++)
+		{
+			  if(min > a[i])
+			  {
+			  	  min = a[i];
+			  	  
+			  }
+		}
+		System.out.println(min);	// your code goes here
+	}
+}
